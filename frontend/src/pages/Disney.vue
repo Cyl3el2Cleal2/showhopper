e49ade90f788d8ade38f86323543b35d63a7172d
macmini <macmini@macminis-Mini.lan>
2021-05-13 02:00:49 +0700

    Init frontend

---
+++ frontend/src/pages/Disney.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+      <a href="/movies">Movies</a>
+      <a href="/series"></a>
+  </nav>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Disney',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  }
+})
+</script>
+
+<style>
+
+</style>
