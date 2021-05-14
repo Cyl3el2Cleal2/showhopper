edce72bc092c47eb3b2997df3a2e07dbb20cefe9
Cyl3el2Cleal2 <natha_sk@hotmail.com>
2021-05-14 22:11:43 +0700

    redesign top bar

--- frontend/src/pages/Disney.vue
+++ frontend/src/pages/Disney.vue
@@ -35,12 +35,60 @@
     </div>
   </nav>
   <div
-    class="container px-6 py-4 mt-4 rounded-lg shadow-md mx-auto bg-gray-200"
+    class="container px-6 py-1 mt-4 rounded-lg shadow-md mx-auto bg-gray-200"
   >
-    <div class="grid gap-8 mt-6 sm:grid-cols-2 md:grid-cols-4">
+    <div class="mt-6 flex space-x-2 place-content-center">
+      <!-- component -->
+      <div class="relative flex-1">
+        <input
+          type="text"
+          class="bg-purple-white shadow-md rounded border-0 py-2 px-3 w-full"
+          placeholder="Search by name..."
+        />
+        <div class="absolute right-3 top-3 text-purple-lighter">
+          <svg
+            version="1.1"
+            class="h-4 text-dark"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 52.966 52.966"
+            style="enable-background: new 0 0 52.966 52.966"
+            xml:space="preserve"
+          >
+            <path
+              d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
+        c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
+        C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
+        S32.459,40,21.983,40z"
+            />
+          </svg>
+        </div>
+      </div>
+      <button
+        type="button"
+        class="px-4 py-2 font-medium tracking-wide text-white capitalize bg-blue-600 rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500 shadow-md"
+        @click="getDisney(filter.type)"
+      >
+        Search
+      </button>
+      <button
+        type="button"
+        @click="showFilter = !showFilter"
+        class="shadow-md rounded-lg hover:shadow-xl w-10 h-10 bg-purple-400 hover:bg-purple-300 focus:outline-none"
+      >
+        <img src="/ico/filter.svg" class="w-full h-full" alt="Filter" />
+      </button>
+    </div>
+
+    <div
+      class="flex flex-row flex-wrap justify-items-stretch md:justify-center gap-x-4 gap-y-2 mt-2"
+      v-if="showFilter"
+    >
       <select
         v-model="filter.genre"
-        class="border-gray-300 px-1 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        class="border-gray-300 px-1 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 sm:flex-grow md:flex-grow-0"
       >
         <option value="">All Genre</option>
         <option v-for="genre in metaGenre" :value="genre" :key="genre">
@@ -60,14 +108,9 @@
           {{ language }}
         </option>
       </select>
-      <button
-        class="px-4 py-2 font-medium tracking-wide text-white capitalize bg-blue-600 rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-        @click="getDisney(filter.type)"
-      >
-        Search
-      </button>
     </div>
-    <div class="text-right text-sm">{{ disneyCount }} results</div>
+
+    <div class="text-right text-sm mt-2">{{ disneyCount }} results</div>
 
     <div
       class="w-full grid gap-x-2.5 gap-y-4 grd-flow-col sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
@@ -105,6 +148,8 @@ export default defineComponent({
     const metaGenre = ref([]);
 
     const disneyCount = ref(0);
+
+    let showFilter = ref(false);
 
     const filter = reactive({
       type: "",
@@ -193,6 +238,7 @@ export default defineComponent({
       metaGenre,
       goToPage,
       navClick,
+      showFilter,
     };
   },
 });
